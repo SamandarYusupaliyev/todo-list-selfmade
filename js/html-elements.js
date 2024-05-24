476c5947b798d37dd9c84b020e31c9a32bbd6da2
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-05-24 23:57:11 +0500

    Select audios

--- js/html-elements.js
+++ js/html-elements.js
@@ -1,3 +1,13 @@
-const elLoader = document.getElementById("loader");
+const elLoader = document.getElementById("loader"),
+  elAudioRemoveTask = document.getElementById("audioRemoveTask"),
+  elAudioSelectStatus = document.getElementById("audioSelectStatus"),
+  elAudioCheck = document.getElementById("audioCheck"),
+  elAudioAddNewTask = document.getElementById("audioAddNewTask");
 
-export { elLoader };
+export {
+  elLoader,
+  elAudioRemoveTask,
+  elAudioSelectStatus,
+  elAudioCheck,
+  elAudioAddNewTask,
+};
