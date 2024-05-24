05c33fbb7213ceef695a92f1ec9ac937d7a387b1
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-05-25 03:31:16 +0500

    Add theme toggler

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 import cssClassModifiers from "./css-class-modifiers.js";
+import { elThemeToggler } from "./html-elements.js";
 import loader from "./loader.js";
 
 // Loader
 window.onload = () => {
   const { timeout_1000 } = cssClassModifiers;
+  const html = document.documentElement;
+  html.dataset.theme = localStorage.getItem("theme");
   setTimeout(() => {
     loader();
   }, timeout_1000);
+};
+
+// Theme change
+elThemeToggler.onclick = () => {
+  const html = document.documentElement;
+  const currentTheme = html.dataset.theme;
+  html.dataset.theme = currentTheme === "dark" ? "light" : "dark";
+  // Save to localStorage
+  localStorage.setItem("theme", html.dataset.theme);
 };
