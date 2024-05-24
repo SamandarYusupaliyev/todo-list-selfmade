bc1df892f9c61e58ee6054c9f388eb126362903e
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-05-25 03:31:00 +0500

    Add audio elements and form elements

--- js/html-elements.js
+++ js/html-elements.js
@@ -1,13 +1,19 @@
 const elLoader = document.getElementById("loader"),
+  elThemeToggler = document.getElementById("themeToggler"),
   elAudioRemoveTask = document.getElementById("audioRemoveTask"),
   elAudioSelectStatus = document.getElementById("audioSelectStatus"),
   elAudioCheck = document.getElementById("audioCheck"),
-  elAudioAddNewTask = document.getElementById("audioAddNewTask");
+  elAudioAddNewTask = document.getElementById("audioAddNewTask"),
+  elNewTodoForm = document.getElementById("newTodoForm"),
+  elTodos = document.getElementById("todos");
 
 export {
   elLoader,
+  elThemeToggler,
   elAudioRemoveTask,
   elAudioSelectStatus,
   elAudioCheck,
   elAudioAddNewTask,
+  elNewTodoForm,
+  elTodos,
 };
